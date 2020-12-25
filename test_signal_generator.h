e2823030a2ed33a72638efcf964f3ac4a4451f3a
Ben Passman <ben.passman@outlook.com>
2020-12-25 22:55:12 +1300

    Abstracted menu code to use item list from header file.

--- test_signal_generator.h
+++ test_signal_generator.h
@@ -1,3 +1,5 @@
+#define EDIT_MENU_LIST { "Edit settings", "Add new wave", "Modify existing wave", "Delete wave", "Export combined waveform" }
+
 #define MENU_LINES 5
 #define MENU_MESSAGE_Y MENU_LINES + 1
 
